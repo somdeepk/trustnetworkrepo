cbf6195bdd78f7210943a1ce02b631405d12904e
Anirban Mandal <sketch.desg07@gmail.com>
2021-10-05 19:43:48 +0530

    css change

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -1,17 +1,11 @@
 <section class="sign-in-page" ng-controller="loginController" ng-init="getLoginData();">
     <div class="container">
+        <div class="sign-up-inner">
         <div class="row no-gutters">
-            <div class="col-md-6 text-center">
-                <div class="sign-in-detail text-white">
-                    <a class="sign-in-logo" href="#"><img src="<?php echo base_url();?>assets/images/logo-full.png" class="img-fluid" alt="logo"></a>
-                    <p style="color:#000;">Successful ministry in the 21st Century is evolving at a rapid rate. We believe it will take a delicate combination of both technolgy and biblical teaching to continuing growing body the body of Christ and the individuals who attend the local church.</p>
-                    <p  style="color:#000;"><em style="color:#0391c0; font-weight:400;">The FOLLOW ME NOW-Making Disciples</em> web-based six-month module systems help churches disciple individuals.</p>
-                    
-                </div>
-            </div>
-            <div class="col-md-6 bg-white pt-5">
+            <div class="col-md-6 order-md-2 pt-0">
                 <div class="sign-in-from">
-                    <h8 class="mb-0">Sign in</h8>
+                    <a class="sign-in-logo d-block d-md-none" href="#"><img src="<?php echo base_url();?>assets/images/logo-full.png" class="img-fluid" alt="logo"></a>
+                    <h2 class="mb-0"><strong>Sign in</strong></h2>
                     <p>Enter your email address and password to access admin panel.</p>
                     <form class="mt-4">
                         <div class="form-group">
@@ -23,7 +17,7 @@
                         </div>
                         <div class="form-group">
                             <label for="exampleInputPassword1">Password</label>
-                            <a href="#" class="float-right">Forgot password?</a>
+                            <a href="#" class="float-right"><strong>Forgot password?</strong></a>
                             <input type="password" class="form-control mb-0" ng-model="loginData.password" id="password" placeholder="Password">
                             <div class="col-md-12 padding-lr0" style="color:#d43f3a;" >{{(loginDataCheck==true && isNullOrEmptyOrUndefined(loginData.password)==true)? 'Password Required' : ''}}</div>
 
@@ -38,17 +32,27 @@
                             <button type="button" class="btn btn-primary float-right" ng-click="submitLogin();">Sign in</button>
                         </div>
                         <div class="sign-info">
-                            <span class="dark-color d-inline-block line-height-2">Don't have an account? <a href="<?php echo base_url();?>user/signup">Sign up</a></span>
+                            <span class="dark-color d-inline-block line-height-2">Don't have an account? <a href="<?php echo base_url();?>user/signup"><strong>Sign up</strong></a></span>
                             <ul class="iq-social-media">
                                 <li><a href="#"><i class="ri-facebook-box-line"></i></a></li>
                                 <li><a href="#"><i class="ri-google-line"></i></a></li>
                                 <li><a href="#"><i class="ri-twitter-line"></i></a></li>
-                                <li><a href="#"><i class="ri-instagram-line"></i></a></li>
+                                <!-- <li><a href="#"><i class="ri-instagram-line"></i></a></li> -->
                             </ul>
                         </div>
                     </form>
                 </div>
             </div>
+            <div class="col-md-6 text-center">
+                <div class="sign-in-detail text-white">
+                    <a class="sign-in-logo" href="#"><img src="<?php echo base_url();?>assets/images/logo-full.png" class="img-fluid" alt="logo"></a>
+                    <p style="color:#000;">Successful ministry in the 21st Century is evolving at a rapid rate. We believe it will take a delicate combination of both technolgy and biblical teaching to continuing growing body the body of Christ and the individuals who attend the local church.</p>
+                    <p  style="color:#000;"><em style="color:#0391c0; font-weight:400;">The FOLLOW ME NOW-Making Disciples</em> web-based six-month module systems help churches disciple individuals.</p>
+                    
+                </div>
+            </div>
+            
         </div>
+    </div>
     </div>
 </section>
